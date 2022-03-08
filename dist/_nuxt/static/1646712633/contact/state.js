68e00adda8842f3042a4723e6beaa567295ae164
Fakhri17 <fakhricoffe@gmail.com>
2022-03-08 11:12:10 +0700

    fix some error

---
+++ dist/_nuxt/static/1646712633/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flearn-nuxtjs\u002F_nuxt\u002Fstatic\u002F1646712633",layout:"DefaultBase",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002Flearn-nuxtjs\u002F",assetsPath:"\u002Flearn-nuxtjs\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a4563dfc"}}}}(null));
